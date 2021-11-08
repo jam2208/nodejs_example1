4db642e09a60d57ea14d573bf67add2097bbf12f
jam2208 <junwoo.park82@icloud.com>
2021-11-08 10:58:20 +0900

    create callback func.

--- server.js
+++ server.js
@@ -4,5 +4,8 @@
 const http = require('http');
 
 // http 모듈의 createServer 메소드를 호출하여 HTTP 서버 생성
-const server = http.createServer();
-server.listen(8888);
+http.createServer(function (request, response) {
+    response.writeHead(200, {'Content-Type': 'text/plain'}); // (1)
+    response.write('Hello World');
+    response.end();
+}).listen(8888);
