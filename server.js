90943bacbfa9d32bfb22d43137f230c570566b93
jam2208 <junwoo.park82@icloud.com>
2021-11-08 16:43:10 +0900

    Separate requests based on url path

--- server.js
+++ server.js
@@ -4,7 +4,10 @@ const http = require('http'); // node.js의 내부모듈
 
 function start() {
     function onRequest(request, response) {
-        console.log('Request received.');
+        const baseUrl = 'http://' + request.headers.host + '/';
+        const newUrl = new URL(request.url, baseUrl);
+        const pathname = newUrl.pathname;
+        console.log('Request for ' + pathname + ' received.');
         response.writeHead(200, {'Content-Type': 'text/plain'}); // (1)
         response.write('Hello World');
         response.end();
