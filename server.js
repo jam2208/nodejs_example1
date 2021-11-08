8277a7cd28db0e245d5999ab924a3e4273747764
jam2208 <junwoo.park82@icloud.com>
2021-11-08 17:41:09 +0900

    dependency injection: connect server and router

--- server.js
+++ server.js
@@ -2,12 +2,15 @@
 
 const http = require('http'); // node.js의 내부모듈
 
-function start() {
+function start(route) {
     function onRequest(request, response) {
         const baseUrl = 'http://' + request.headers.host + '/';
         const newUrl = new URL(request.url, baseUrl);
         const pathname = newUrl.pathname;
         console.log('Request for ' + pathname + ' received.');
+
+        route(pathname);
+
         response.writeHead(200, {'Content-Type': 'text/plain'}); // (1)
         response.write('Hello World');
         response.end();
