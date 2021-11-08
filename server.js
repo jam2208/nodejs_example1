e1fe8279756da2379322c82e40abcf097c64acbd
jam2208 <junwoo.park82@icloud.com>
2021-11-08 11:18:05 +0900

    functionalize variable :function(req., res) -> onRequest(req., res)

--- server.js
+++ server.js
@@ -4,8 +4,13 @@
 const http = require('http');
 
 // http 모듈의 createServer 메소드를 호출하여 HTTP 서버 생성
-http.createServer(function (request, response) {
+function onRequest(request, response) {
+    console.log('Request received.');
     response.writeHead(200, {'Content-Type': 'text/plain'}); // (1)
     response.write('Hello World');
     response.end();
-}).listen(8888);
+}
+
+http.createServer(onRequest).listen(8888);
+
+console.log('Server has started.');
