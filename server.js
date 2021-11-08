7fdcd74b3ab5bc4b4be715a5ac5952fc7c5bcb2b
jam2208 <junwoo.park82@icloud.com>
2021-11-08 11:27:25 +0900

    http server creation modelarization

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 // server.js
 
-// Node.js에 기본 내장되어 있는 http 모듈을 로드한다.
-const http = require('http');
+const http = require('http'); // node.js의 내부모듈
 
-// http 모듈의 createServer 메소드를 호출하여 HTTP 서버 생성
-function onRequest(request, response) {
-    console.log('Request received.');
-    response.writeHead(200, {'Content-Type': 'text/plain'}); // (1)
-    response.write('Hello World');
-    response.end();
+function start() {
+    function onRequest(request, response) {
+        console.log('Request received.');
+        response.writeHead(200, {'Content-Type': 'text/plain'}); // (1)
+        response.write('Hello World');
+        response.end();
+    }
+
+    http.createServer(onRequest).listen(8888);
+    console.log('Server has started.');
 }
 
-http.createServer(onRequest).listen(8888);
-
-console.log('Server has started.');
+exports.start = start;
